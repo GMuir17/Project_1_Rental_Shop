4a6f6c2e42d125ac6cc45e87f115ca78245c752b
Gary Muir <garymuir17@gmail.com>
2018-06-18 11:42:46 +0100

    attempted to make seed file, realised need models first

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+# require_relative()
+require("pry")
+
+# delete_all()
+
+legion1
--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -2,7 +2,7 @@ require("pg")
 
 class SqlRunner
 
-  def sel.run(sql, values = [])
+  def self.run(sql, values = [])
     begin
       db = PG.connect({
         dbname: "legion_deployment",
