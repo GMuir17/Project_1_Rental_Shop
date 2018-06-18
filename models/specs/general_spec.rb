99c496000a75aa7365826a77cfafc7f2bb5ba07a
Gary Muir <garymuir17@gmail.com>
2018-06-18 12:19:22 +0100

    attr_reader methods tested for General class

--- models/specs/general_spec.rb
+++ models/specs/general_spec.rb
@@ -5,6 +5,7 @@ class GeneralTest < MiniTest::Test
 
   def setup()
     options = {
+      "id" => 2,
       "name" => "Marcus Agrippa",
       "reputation" => 10
     }
@@ -13,6 +14,14 @@ class GeneralTest < MiniTest::Test
 
   def test_has_name()
     assert_equal("Marcus Agrippa", @general1.name())
+  end
+
+  def test_has_id()
+    assert_equal(2, @general1.id())
+  end
+
+  def test_has_reputation()
+    assert_equal(10, @general1.reputation())
   end
 
 end
