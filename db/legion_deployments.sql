a32992ca02e351b881add8fc61abc1553ad622b0
Gary Muir <garymuir17@gmail.com>
2018-06-18 10:57:45 +0100

    tables created in legion_deployments.sql

---
+++ db/legion_deployments.sql
@@ -0,0 +1,29 @@
+DROP TABLE deployments;
+DROP TABLE legions;
+DROP TABLE generals;
+
+CREATE TABLE legions
+(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  moto VARCHAR(255),
+  strength INT4
+);
+
+CREATE TABLE generals
+(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  province VARCHAR(255),
+  reputation INT4
+);
+
+CREATE TABLE deployments
+(
+  id SERIAL8 PRIMARY KEY,
+  legion_id INT4 REFERENCES legions(id),
+  general_id INT4 REFERENCES generals(id),
+  start_date VARCHAR(255),
+  campaign_length INT4,
+  returned BOOLEAN DEFAULT FALSE
+);
