758223a292018cf2b38a4f0a169228d5800bce1c
Gary Muir <garymuir17@gmail.com>
2018-06-19 10:38:07 +0100

    will add the index route to Deployment

--- controllers/deployments_controller.rb
+++ controllers/deployments_controller.rb
@@ -4,3 +4,9 @@ require_relative("../models/legion.rb")
 require_relative("../models/general.rb")
 require_relative("../models/deployment.rb")
 also_reload("../models/*")
+
+# index
+get "/deployments" do
+  @deployments = Deployment.all()
+  erb(:"/deployments/index")
+end
