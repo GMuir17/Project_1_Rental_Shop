b5c58e924e432100d5f251f2c117b4bacc3cdd4f
Gary Muir <garymuir17@gmail.com>
2018-06-19 09:53:07 +0100

    will add the index route to Generals

--- controllers/generals_controller.rb
+++ controllers/generals_controller.rb
@@ -2,3 +2,47 @@ require("sinatra")
 require("sinatra/contrib/all")
 require_relative("../models/legion.rb")
 also_reload("../models/*")
+
+# index
+get "/generals" do
+  @generals = General.all()
+  erb(:"generals/index")
+end
+
+# # new
+# get "/legions/new" do
+#   erb(:"legions/new")
+# end
+#
+# # show
+# get "/legions/:id" do
+#   @legion = Legion.find_by_id(params["id"].to_i())
+#   erb(:"legions/show")
+# end
+#
+# # create
+# post "/legions" do
+#   @legion = Legion.new(params)
+#   @legion.save()
+#   erb(:"legions/create")
+# end
+#
+# # delete
+# post "/legions/:id/delete" do
+#   @legion = Legion.find_by_id(params["id"].to_i())
+#   @legion.delete()
+#   redirect "/legions"
+# end
+#
+# # edit
+# get "/legions/:id/edit" do
+#   @legion = Legion.find_by_id(params["id"].to_i())
+#   erb(:"legions/edit")
+# end
+#
+# # update
+# post "/legions/:id" do
+#   @legion = Legion.new(params)
+#   @legion.update()
+#   redirect "/legions/#{@legion.id()}"
+# end
