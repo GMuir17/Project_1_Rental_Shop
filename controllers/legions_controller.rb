c8f7d108a3e0e332f8b11f382ea3e892a46685b7
Gary Muir <garymuir17@gmail.com>
2018-06-19 08:41:57 +0100

    show route done for Legions

--- controllers/legions_controller.rb
+++ controllers/legions_controller.rb
@@ -3,7 +3,14 @@ require("sinatra/contrib/all")
 require_relative("../models/legion.rb")
 also_reload("../models/*")
 
+# index
 get "/legions" do
   @legions = Legion.all()
   erb(:"legions/index")
+end
+
+# show
+get "/legions/:id" do
+  @legion = Legion.find_by_id(params["id"].to_i())
+  erb(:"legions/show")
 end
