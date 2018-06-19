3a3908554ab67817da1371773858ce6718c37338
Gary Muir <garymuir17@gmail.com>
2018-06-19 08:37:44 +0100

    legions_controller and index setup

--- controllers/legions_controller.rb
+++ controllers/legions_controller.rb
@@ -2,3 +2,8 @@ require("sinatra")
 require("sinatra/contrib/all")
 require_relative("../models/legion.rb")
 also_reload("../models/*")
+
+get "/legions" do
+  @legions = Legion.all()
+  erb(:"legions/index")
+end
