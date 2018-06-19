e0c1f0a85e165c7f5f76e01010db99d247165f7a
Gary Muir <garymuir17@gmail.com>
2018-06-19 09:30:22 +0100

    will add the edit route and erb to Legion

--- controllers/legions_controller.rb
+++ controllers/legions_controller.rb
@@ -19,3 +19,16 @@ get "/legions/:id" do
   @legion = Legion.find_by_id(params["id"].to_i())
   erb(:"legions/show")
 end
+
+# create
+post "/legions" do
+  @legion = Legion.new(params)
+  @legion.save()
+  erb(:"legions/create")
+end
+
+# edit
+get "/legions/:id/edit" do
+  @legion = Legion.find_by_id(params["id"].to_i())
+  erb(:"legions/edit")
+end
